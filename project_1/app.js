7aefe598af77661fab5547501657747e1ecc7563
Noah B. Howe <noahbhowe@gmail.com>
2017-01-27 15:52:31 -0500

    fundamentally restructuring code to within a single instance of a function Tron help us.

--- project_1/app.js
+++ project_1/app.js
@@ -1,12 +1,42 @@
-USE A FOR-LOOP to cycle through
 
-so the tree would look like this:
 
 
 
 'use strict';
 
 $(document).ready(function() {
+
+var randomTime = Math.ceil(Math.random()*5);
+//made it global so that I can redefine inside the funciton (oy)
+var conundrum = setInterval(function() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
 // var $(verde) = function () {
 
 //clears all classes from #courseOneLight
@@ -18,6 +48,7 @@ $(document).ready(function() {
 
 // sets 's' key as keydown for moving car
     $('input').on('keypress', function(verde) {event.which === 83}) {
+
 
 
   }
---
+++ project_1/untitled.js
@@ -0,0 +1,33 @@
+$('input').on('keydown', function(event) {
+   if ($('input').val() !=='' && event.which === 13) {
+
+
+
+
+   }
+
+
+   $("input").keypress(function(event){
+   event.which === 83} {
+
+
+
+
+   }
+
+
+
+};
+var Victory = {left value of carOne}
+
+
+
+
+while ($Victory <= 1000px) {
+  function setTimeout(verde, (Math.random));
+  function setTimeout(amarillo, (Math.random));
+  function setTimeout(rojo, (Math.random));
+}
+
+setTimeout(verde, (Math.random))
+
