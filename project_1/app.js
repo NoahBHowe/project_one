5b47bb50ed6f84e8185bb13298e969b1763af2a1
Noah B. Howe <noahbhowe@gmail.com>
2017-01-29 10:35:04 -0500

    reformatting the jquery equations so they rely on a counter rather than the failed for each loop.

--- project_1/app.js
+++ project_1/app.js
@@ -1,111 +1,61 @@
-
-
-
-
-'use strict';
-//as per convention
-
 $(document).ready(function() {
-//
+
+var $randomTime = Math.ceil((Math.random()*5000));
 
 
-var $randomTime = Math.ceil(Math.random()*5);
-//made it global so that I can redefine inside the function (oy)
-
-var $color = ['green', 'yellow', 'red'];
-//array to cue in the functions
 
 
-//The functions below are at the heart of the thing: they
-// 1) propel the car forward at a steady pace (green)
-// 2) propel the car forward quickly (yellow) and
-// 3) propel the car _backwards_ (red).
-// See the diagramed sample below to see how!:
-
-// var verde = function () { //<-name the function
-//   $("#courseOneLight").removeClass(); //<-clears previous classes
-//   $("#courseOneLight").addClass('.greenLight'); //<-adds a new class
-//   $(window).on('keydown', function(event) { //<-change the activity of the key press events
-//      (event.which === 83) //<-selects the key that performs the action ('s' key)
-//       $("#carOne").animate ({. //<-selects the action that will be performed
-//         "left" : "+=10px", //<-moves the 'div with the id 'carOne' ten px right per key press
-//         });
-//       });
-// };
 
 
-var $verde = function () {
+
+});
+
+
+
+
+
+
+
+
+var $goGreen = function (){
+var timesRunVerde = 0;
+var $verde = setInterval(function(){
+    timesRunVerde += 1;
+    if(timesRunVerde === 2){
+        clearInterval($verde);
+    }
   $("#courseOneLight").removeClass();
   $("#courseOneLight").addClass('.greenLight');
   $(window).off();
   $(window).on('keydown', function(event) {
-   (event.which === 83)
+   (event.which === 83);
    $("#carOne").animate ({
     "left" : "+=10px",
   });
  });
+}), $randomTime;
+verde();
 };
 
-var $amarillo = function () {
+
+var $goYellow = function (){
+var timesRunAmarillo = 0;
+var $amarillo = setInterval(function(){
+    timesRunAmarillo += 1;
+    if(timesRunAmarillo === 2){
+        clearInterval($amarillo);
+    }
   $("#courseOneLight").removeClass();
   $("#courseOneLight").addClass('.yellowLight');
   $(window).off();
   $(window).on('keydown', function(event) {
-   (event.which === 83)
+   (event.which === 83);
    $("#carOne").animate ({
     "left" : "+=20px",
   });
  });
+}), $randomTime;
+amarillo();
 };
 
-var $rojo = function () {
-  $("#courseOneLight").removeClass();
-  $("#courseOneLight").addClass('.redLight');
-  $(window).off();
-  $(window).on('keydown', function(event) {
-   (event.which === 83)
-   $("#carOne").animate ({
-    "left" : "-=15px",
-  });
 
- });
-};
-
-var $conundrum = function () {
-  setInterval(function() {
-  $randomTime = (Math.ceil(Math.random()*5));
-  for (var i = 0, len = $color.length; i < len; i++) {
-    var $snorkel = function () {$color[i]};
-    $snorkel();
-    if ($snorkel === 'green') {
-      $verde();
-    } else if
-    ($snorkel === 'yellow') {
-      $amarillo();
-    } else {
-      $rojo();
-    }
-
- //victory conditions below!
- //the victory code is modified from an alert model posted on: http://stackoverflow.com/questions/7875592/fire-event-if-margin-left-200px
-     var $victory = 0;
-      setInterval(function(){
-        if ($("#carOne").css("left")==="1000px" && $victory===0) {
-        alert("You beat the Gridlock!");
-        $victory=1;
-      }
-        else {
-        $victory=0;
-          }
-      },
-      1000);
-    }(1000*$randomTime);
-  });
-};
-
-$(window).on('keydown', function(event) {
-   (event.which === 84);
-    $conundrum();  //gotta call the function!
-});
-
-});
